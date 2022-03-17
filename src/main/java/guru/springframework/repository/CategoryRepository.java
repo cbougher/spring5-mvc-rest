c44bda97a9ffc0cc5352591aab8e52bfc87e2005
Chris Bougher <cbougher@gmail.com>
2022-03-17 18:36:22 -0400

    first commit

---
+++ src/main/java/guru/springframework/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.repository;
+
+import guru.springframework.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
